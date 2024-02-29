78e3d1ee5aeda678f46c6d82454c9379d8cc932d
Abdelrahman Fouad <153091283+Abdelrahman1994Ahmed@users.noreply.github.com>
2024-02-29 20:22:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+// let ans1 = "yes";
+// let ans2 = "no";
+
+// let ansg1 = prompt("do you fly ?");
+// if(ansg1 == ans1){
+//     let ansg2 = prompt("are you wild ?");
+//     if (ansg2 == ans1){
+//         console.log("Eagle");
+//     }else if(ansg2 == ans2){
+//         console.log("Parrot");
+//     }else{
+//     }
+// }else if(ansg1 == ans2){
+//     let ansg3 = prompt("Do you live under sea ?")
+//     if (ansg3 == ans1){
+//         let ansg4 = prompt("Are you wild ?")
+//         if (ansg4 == ans1){
+//             console.log("Shark");
+//         }else if (ansg4 == ans2){
+//             console.log("Dolphine");
+//         }else{};
+//     }else if(ansg3 == ans2){
+//         let ansg5 = prompt("Are you wild ?")
+//         if (ansg5 == ans1){
+//             console.log("Lion");
+//         }else if (ansg5 == ans2){
+//             console.log("Cat");
+//         }else{};
+//     }else{}
+// }else{}
+
+// ---------------------------------------------------
+// let arr1 = [3,2,5,9,4];
+// let arrg = [2,3,5,9,6];
+
+// function summation (values) {
+//     let sum = 0
+//     for (let i = 0; i < values.length; i++) {
+//     sum += values[i];
+//     }
+//     return sum
+// }
+
+// console.log(summation(arr1));
+// console.log(summation(arrg));
+
+// ---------------------------------------------------
+
+// let variable3 = (' a h m e ds ');
+
+
+// console.log(variable3.trim()); 
+
+// ----------------------------------
+
+// let value = Number(prompt("Insert a number :"));
+
+// if (value % 10 == 0) {
+//     console.log("true");
+// }else{
+//     console.log("not divisible");
+// }
+
+// --------------------------------------------------
+
+// let value1 = Number(prompt("Insert a number1 :"));
+// let value2 = Number(prompt("Insert a number2 :"));
+
+// let bigger_value = value1 > value2 ? value1 : value2;
+
+// console.log(bigger_value);
+
+// // or
+
+// // let bigger_value = value1 > value2 ? "first number is bigger" : "second number is bigger";
+
+// console.log(bigger_value);
+
+// --------------------------------
+
+// 1- string type
+
+// 2- let variable6 =('ahmed');
+// let variable7 =7;
+// let ans = variable6 + variable7
+// // the ans will be ahmed7
+
+// console.log(ans);
+
+// 3- and in conditions, which means that 1st condition and 2nd condition must be happining
+
+// ---------------------------------
+
+// let arr_f = [1,5,6,3,2,'f',3];
+
+// arr_f.forEach()
